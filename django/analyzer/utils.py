87d1db42b62b9a601f674b26dd4e090f8bdddfb0
Matthew Caulfield <matthew.caulfield@zoho.com>
2020-12-14 21:36:22 -0600

    Added util stubs and test sentiment analysis

---
+++ django/analyzer/utils.py
@@ -0,0 +1,11 @@
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+nltk.download('vader_lexicon')
+analyzer = SentimentIntensityAnalyzer()
+
+def get_articles():
+    return {}
+
+def get_sentiment(article):
+    return analyzer.polarity_scores(article)
--- django/analyzer/views.py
+++ django/analyzer/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from analyzer.utils import get_sentiment
 
 def analyze(request):
-    return HttpResponse('Sentiment analysis')
+    return HttpResponse(get_sentiment("This is a test of happy sentiment.")['compound'])
